a1c204d1396f17d2a5ff255778522d86eed91435
Abdelhadi Chougar <chougar.abdelhadi@gmail.com>
2024-01-10 16:36:06 +0100

    math

---
+++ 02_POO_Python/05_Math_Python .py
@@ -0,0 +1,26 @@
+# Math
+
+x = max(23, 24,22)
+y = min(23, 24,22)
+print(x)
+print(y)
+
+x = abs(-7.25)
+print(x) # 7.25
+
+x = pow(2, 3)
+print(x) # 2^3 = 8
+
+import math
+
+print("** ceil  **")
+x = math.sqrt(19)
+print(x) # 4.358898943540674
+x = math.ceil(x)
+print(x) # 5
+print("** floor **")
+x = math.sqrt(19)
+print(x) # 4.358898943540674
+x = math.floor(x)
+print(x) # 4
+
