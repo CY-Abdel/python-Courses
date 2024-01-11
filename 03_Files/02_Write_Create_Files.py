4078ef801c1bfb6b403ee3313b25a640948a0dad
Abdelhadi Chougar <chougar.abdelhadi@gmail.com>
2024-01-11 09:39:33 +0100

    append, write and create Files

--- 03_Files/02_Write_Create_Files.py
+++ 03_Files/02_Write_Create_Files.py
@@ -4,8 +4,26 @@
 """
 To write to an existing file, you must add a parameter to the open() function:
 
-"a" - Append - will append to the end of the file
+"a" - Append - will append to the end of the file / creates the file if it does not exist
 
-"w" - Write - will overwrite any existing content
-
+"w" - Write - will overwrite any existing content / creates the file if it does not exist
 """
+f = open("Assets/demofile2.txt", "a")
+f.write("\nNow the file has more content!")
+f.close()
+
+# ****** open and read the file after the appending:
+f = open("Assets/demofile2.txt", "r")
+print(f.read())
+f.close()
+print()
+
+# ******** overwrite the content
+f = open("Assets/demofile2.txt", "w")
+f.write("Now the file overwrite any existing content!")
+f.close()
+
+f = open("Assets/demofile2.txt", "r")
+print(f.read())
+f.close()
+print()
