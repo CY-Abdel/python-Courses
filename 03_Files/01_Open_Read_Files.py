c19ffc1564ba687cb7d5f581f1e6e1a51d452fd3
Abdelhadi Chougar <chougar.abdelhadi@gmail.com>
2024-01-11 09:29:42 +0100

    Files open read and close

---
+++ 03_Files/01_Open_Read_Files.py
@@ -0,0 +1,53 @@
+# ********* Python File Open
+"""The key function for working with files in Python is the open() function.
+
+The open() function takes two parameters; filename, and mode.
+
+There are four different methods (modes) for opening a file:
+
+"r" - Read - Default value. Opens a file for reading, error if the file does not exist
+
+"a" - Append - Opens a file for appending, creates the file if it does not exist
+
+"w" - Write - Opens a file for writing, creates the file if it does not exist
+
+"x" - Create - Creates the specified file, returns an error if the file exists"""
+
+fichier = open("Assets/demofile.txt")
+print(fichier.read())
+fichier.close()
+print()
+
+fichier = open("Assets/demofile.txt", "r")
+print(fichier.read())
+fichier.close()
+print()
+
+# ****** Read Only Parts of the File
+fichier = open("Assets/demofile.txt", "r")
+print(fichier.read(5)) # Return the 5 first characters of the file
+fichier.close() # Bienv
+print()
+
+# ****** Read Only Parts of the File
+fichier = open("Assets/demofile.txt", "r")
+print(fichier.read(4)) # Return the 4 first characters of the file => Bien
+print(fichier.read(5)) # Return the 5 characters after the 4th first characters of the file => venue
+fichier.close()
+print("\n******* readline *******")
+
+# ************** Read Lines
+fichier = open("Assets/demofile.txt", "r")
+print(fichier.readline())
+print(fichier.readline())
+print(fichier.readline())
+fichier.close()
+print()
+
+# By looping through the lines of the file, you can read the whole file, line by line:
+fichier = open("Assets/demofile.txt", "r")
+for ligne in fichier:
+  print(ligne)
+fichier.close()
+print()
+
